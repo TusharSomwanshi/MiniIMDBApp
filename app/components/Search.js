520222491ac2d4d35c7f8683d874f1a2dcf49822
tushar somwanshi <somwanshi.tushar@gmail.com>
2017-02-24 09:11:21 +0530

    working app

--- app/components/Search.js
+++ app/components/Search.js
@@ -8,71 +8,105 @@ class Search extends React.Component{
     this.state = {
       result: [],
       error: false,
-      showModal: false
+      showModal: false,
+      selectedMovie: false
     };
     this.handleOnChange = this.handleOnChange.bind(this);
-    this.getMovieDetails = this.getMovieDetails.bind(this);
+    this.getMovieName = this.getMovieName.bind(this);
     this.close = this.close.bind(this);
     this.open = this.open.bind(this);
+    this.showMovieDetails = this.showMovieDetails.bind(this);
   }
 
   close() {
-    this.setState({ showModal: false });
+    this.setState({
+      selectedMovie: false,
+      showModal: false
+    });
   }
 
-  open() {
-    const api_key = `6cf70746523d3806298c3e8a3f27fe47`;
-    const { movieName } = this.state;
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${movieName}&page=1&include_adult=false`;
+  open(id) {
+    console.log(id);
+    const movie = this.state.result.filter((el) => el.id === id )
 
-    if(movieName.length > 0){
+    if(movie.length > 0)
+    {
       this.setState({
-        movieName: ''
-      })
-    }
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((result) => this.setState({
-        result: result.results[0],
-        showModal: true,
-      }))
-      .catch((error) =>  this.setState({result: [], error}))
-      this.getMovieDetails();
+        selectedMovie: movie[0],
+        showModal: true
+    })}
   }
 
-  getMovieDetails(){
+  showMovieDetails(){
+    const { selectedMovie: movie } = this.state;
+
+    const src = movie.poster_path === null ? "https://placehold.it/300?text=image+not+avvailable":`https://image.tmdb.org/t/p/w185/${movie.poster_path}`
     return(
       <Modal show={this.state.showModal} onHide={this.close}>
         <Modal.Header closeButton>
-          <Modal.Title>{this.state.result.original_title}</Modal.Title>
+          <Modal.Title>{movie.original_title}</Modal.Title>
         </Modal.Header>
         <Modal.Body style={{textAlign:'center'}}>
-            <img style={{height:300,width:"50%"}} src={`https://image.tmdb.org/t/p/w185/${this.state.result.poster_path}`}/>
-          <p style={{textAlign:'justify'}}><b>Overview: </b>{this.state.result.overview}</p>
-          <p style={{textAlign:'justify'}}><b>Popularity: </b>{this.state.result.popularity}</p>
-          <p style={{textAlign:'justify'}}><b>Vote Count: </b>{this.state.result.vote_count}</p>
-          <p style={{textAlign:'justify'}}><b>Vote Average: </b>{this.state.result.vote_average}</p>
+            <img style={{height:300,width:"50%"}} src={src}/>
+          <p style={{textAlign:'justify'}}><b>Overview: </b>{movie.overview}</p>
+          <p style={{textAlign:'justify'}}><b>Popularity: </b>{movie.popularity}</p>
+          <p style={{textAlign:'justify'}}><b>Vote Count: </b>{movie.vote_count}</p>
+          <p style={{textAlign:'justify'}}><b>Vote Average: </b>{movie.vote_average}</p>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={this.close}>Close</Button>
         </Modal.Footer>
       </Modal>
     )
+
+  }
+
+  getMovieName(){
+    const counter = 0;
+    return(
+
+      <div className="autosuggest">
+        {
+          this.state.result.map((movie,index) => {
+            return(
+              <p key={index} onClick={() => this.open(movie.id)}>
+                {index<5 ? `${movie.original_title}` : '' }
+              </p>
+            )
+          }
+      )}
+      </div>
+    )
+
   }
 
   handleOnChange(event){
-    this.setState({
-      movieName: event.target.value
-    });
+
+    const  movieName = event.target.value
+    const api_key = `6cf70746523d3806298c3e8a3f27fe47`;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${movieName}&page=1&include_adult=false`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => { this.setState({
+        result: result.results,
+        error: false
+      })
+    })
+
+      .catch((error) =>  this.setState({
+        result: [],
+        error: 'Movie not found'
+      }))
   }
 
   render(){
     const { result, error, movieName, showModal } = this.state
     return(
-      <div >
+      <div>
+        { this.showMovieDetails() }
         <Row >
-          <Col xs={12} md={11}>
+          <Col xs={12} md={12}>
             <FormControl
               type="search"
               placeholder="Enter movie name to be searched"
@@ -80,11 +114,10 @@ class Search extends React.Component{
               onChange={this.handleOnChange}
             />&nbsp;
           </Col>
-          <Button bsStyle="primary"  onClick={this.open}>Search</Button>
         </Row>
         <p>
         {
-          result.length !== undefined ? 'Loading...' : this.getMovieDetails()
+          result.length === 0 ? '' : this.getMovieName()
         }
         </p>
       </div>
